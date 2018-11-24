d97dff4a414ffff8eabf95b723e84ce01a2423dd
Ryan Purpura <10505811+superiorbocal@users.noreply.github.com>
2018-11-23 18:53:09 -0800

    Added documentation to learn.py

--- learn.py
+++ learn.py
@@ -3,6 +3,13 @@ from random import shuffle
 
 class CardBag:
     def __init__(self, cards, num_cards, shuffle=True):
+        """
+        Create a new CardBag with the given set of cards, limited by num_cards.
+
+        Uses a bag system, where 10 cards will be put into a bag and shuffled
+        (if shuffle is true, which is default). When there are fewer than
+        3 cards, the bag will be refilled to 10 cards.
+        """
         eligible_cards = list(filter(lambda x: x.is_reviewable(), cards))
 
         if num_cards < 0 or num_cards > len(eligible_cards):
@@ -15,6 +22,7 @@ class CardBag:
         self.shuffle = shuffle
 
     def refill_bag(self):
+        """Refill the bag to 10 cards, if possible."""
         new_cards = self.cards[:10 - len(self.current_bag)]
         del self.cards[len(self.current_bag):10]
 
@@ -24,9 +32,11 @@ class CardBag:
         self.current_bag.extend(new_cards)
 
     def recycle_last(self):
+        """Place the most recent card back into the bag."""
         self.current_bag.append(self.last_card)
 
     def __next__(self):
+        """Get the next card to review."""
         if self.cards and len(self.current_bag) < 3:
             self.refill_bag()
 
@@ -38,10 +48,19 @@ class CardBag:
         return self.last_card
 
     def __iter__(self):
+        """Return iterator over cards, which here is the object per se."""
         return self
 
 
-def learn(cards, show_fn, rate_fn, num_cards=-1):
+def learn(cards, show_fn=None, rate_fn=None, num_cards=-1):
+    """Learn the given cards.
+
+    show_fn is intended to be used to prompt the user for an answer,
+    and rate_fn is used to prompt the user for a result. If show_fn is not
+    given, nothing will occur, and if rate_fn is not given, "3" will be the
+    rating given.
+    """
+
     if num_cards <= 0:
         return
 
